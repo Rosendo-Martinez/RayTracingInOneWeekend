8f9e025b2b1827d77a270df86ea014aebf27754c
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-12-30 22:17:11 -0600

    Ray-sphere intersection

--- main.cpp
+++ main.cpp
@@ -4,9 +4,26 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc,oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r)
 {
-    // Color gradient from blue to white
+    // Color gradient from blue to white for background
+    // And, red pixel color if sphere hit
+
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+    {
+        return color(1,0,0);
+    }
 
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5*(unit_dir.y() + 1.0); // [0,1]
