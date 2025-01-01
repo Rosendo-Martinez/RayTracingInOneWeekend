24804338422544e9eb225f95d0006104e4b86fd5
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-12-31 22:00:17 -0600

    set_face_normal()

--- hittable.h
+++ hittable.h
@@ -9,6 +9,34 @@ public:
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    /**
+     * Figures out proper normal direction, and what side (front or back)
+     * the object was hit.
+     * 
+     * @param r ray that hit hittable object
+     * @param outward_normal OUTWARD normal of hit surface at hit point
+     */
+    void set_face_normal(const ray& r, const vec3& outward_normal)
+    {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        // NOTE: choosen that normal should always go in oppsite direction as 
+        // ray.
+
+        // EXAMPLE with sphere:
+        // Ray was inside sphere if outward_normal is going in same-ish
+        // direction as it (the ray).
+        // Else the ray was outside the sphere, and 
+        // it (ray) is going in oppsite-ish direction as the outward-normal.
+
+        // For sphere front face is its outside, and the back face is its inside.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable
